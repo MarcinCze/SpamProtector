ad40e1a974d1df59be10cc073effd54c9c67c836
Marcin Czernecki <Marcin_Czernecki@epam.com>
2020-08-11 12:34:18 +0200

    working on login page

--- dashboard/spam-protector-dashboard/src/app/components/login/login.component.ts
+++ dashboard/spam-protector-dashboard/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.maxLength(50)])
+  });
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onLoginSubmit() {
+
+  }
 }
