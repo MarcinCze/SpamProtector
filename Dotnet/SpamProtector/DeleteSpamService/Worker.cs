2158b2deccf9847cc734955aa993be42d14ddd2a
Marcin Czernecki <Marcin_Czernecki@epam.com>
2021-01-29 15:33:50 +0100

    changing classes to use ExtendedBackgroundService

--- Dotnet/SpamProtector/DeleteSpamService/Worker.cs
+++ Dotnet/SpamProtector/DeleteSpamService/Worker.cs
@@ -1,78 +1,31 @@
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
 using ProtectorLib.Handlers;
 using ProtectorLib.Providers;
-
-using System;
-using System.Diagnostics;
-using System.Text.Json;
-using System.Threading;
+using ProtectorLib.Services;
 using System.Threading.Tasks;
 
 namespace DeleteSpamService
 {
-    public class Worker : BackgroundService
+    public class Worker : ExtendedBackgroundService
     {
-        private readonly ILogger<Worker> logger;
-        private readonly IServiceRunHistoryHandler serviceRunHistoryHandler;
-        private readonly IServiceRunScheduleProvider serviceRunScheduleProvider;
         private readonly IMailboxProvider mailboxProvider;
 
         public Worker(
             ILogger<Worker> logger,
             IMailboxProvider mailboxProvider,
             IServiceRunHistoryHandler serviceRunHistoryHandler,
-            IServiceRunScheduleProvider serviceRunScheduleProvider)
+            IServiceRunScheduleProvider serviceRunScheduleProvider) : base (logger, serviceRunHistoryHandler, serviceRunScheduleProvider)
         {
-            this.logger = logger;
             this.mailboxProvider = mailboxProvider;
-            this.serviceRunScheduleProvider = serviceRunScheduleProvider;
-            this.serviceRunHistoryHandler = serviceRunHistoryHandler;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override string ServiceName => nameof(DeleteSpamService);
+        protected override string ServiceVersion => GetType().Assembly.GetName().Version?.ToString();
+
+        protected override async Task ExecuteBodyAsync()
         {
-            var stopWatch = new Stopwatch();
-
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-
-                if (serviceRunScheduleProvider.ShouldRun(nameof(DeleteSpamService)))
-                {
-                    logger.LogInformation("Started operation");
-                    stopWatch.Start();
-
-                    string additionalInfo = null;
-                    var status = ServiceRunHistoryHandler.ServiceStatus.PROCESSING;
-
-                    await serviceRunHistoryHandler.RegisterStartAsync(nameof(DeleteSpamService), GetType().Assembly.GetName().Version.ToString());
-
-                    try
-                    {
-                        (int countBefore, int countAfter) = await mailboxProvider.DeleteMessagesAsync();
-                        status = ServiceRunHistoryHandler.ServiceStatus.DONE;
-                        additionalInfo = $"BEFORE: {countBefore} AFTER: {countAfter}";
-                        status = ServiceRunHistoryHandler.ServiceStatus.DONE;
-                    }
-                    catch (Exception ex)
-                    {
-                        status = ServiceRunHistoryHandler.ServiceStatus.ERROR;
-                        additionalInfo = JsonSerializer.Serialize(new { ex.Message, ex.StackTrace });
-                    }
-                    finally
-                    {
-                        stopWatch.Stop();
-                        await serviceRunHistoryHandler.RegisterFinishAsync(nameof(DeleteSpamService), additionalInfo, status, $"{stopWatch.ElapsedMilliseconds} ms");
-                        stopWatch.Reset();
-                    }
-
-                    logger.LogInformation("Service run done");
-                }
-
-                await Task.Delay(new TimeSpan(0, 0, 30), stoppingToken);
-            }
+            (int countBefore, int countAfter) = await mailboxProvider.DeleteMessagesAsync();
+            ServiceResultAdditionalInfo = $"BEFORE: {countBefore} AFTER: {countAfter}";
         }
     }
 }
