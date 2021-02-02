1985f72e4c8c37d7608f39877726ba4827185733
MarcinCze <marcin@mczernecki.pl>
2021-02-02 16:59:37 +0100

    adding vRulesUsage view

---
+++ Dotnet/SpamProtector/ProtectorDatabase/vRulesUsage.sql
@@ -0,0 +1,5 @@
+﻿CREATE VIEW [dbo].[vRulesUsage]
+	AS SELECT dbo.Rules.Id, dbo.RuleTypes.Name AS Type, dbo.Rules.Value, dbo.RuleUsageStats.TimesUsed
+       FROM dbo.Rules 
+       INNER JOIN dbo.RuleTypes ON dbo.Rules.RuleTypeId = dbo.RuleTypes.Id 
+       INNER JOIN dbo.RuleUsageStats ON dbo.Rules.Id = dbo.RuleUsageStats.RuleId
